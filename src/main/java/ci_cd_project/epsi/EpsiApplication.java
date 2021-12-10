b48fa66f12e2a35f41ed5622c467418b14015cbb
Theo Zaranski <theo.zaranski@gmail.com>
2021-12-10 09:44:46 +0100

    Initial commit

---
+++ src/main/java/ci_cd_project/epsi/EpsiApplication.java
@@ -0,0 +1,13 @@
+package ci_cd_project.epsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpsiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpsiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ci_cd_project/epsi/EpsiApplicationTests.java
@@ -0,0 +1,13 @@
+package ci_cd_project.epsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpsiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
